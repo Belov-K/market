3f9a9b34ae6fcabd5b937a8c01004e16dc0d50c0
kiril <kaz1nak3@mail.ru>
2023-06-19 12:25:19 +0300

    fix(products): испавил ошибку pub_date

--- market/apps/products/models.py
+++ market/apps/products/models.py
@@ -1,9 +1,12 @@
+import datetime
 from django.db import models
+
+from django.utils import timezone
 
 class Product(models.Model):
     product_title=models.CharField('название товара', max_length=200)
     product_text=models.TextField('описание товара')
-    pub_date=models.DateTimeField('дата публикации товара')
+    pub_date=models.DateTimeField('дата публикации товара', blank=True, null=True)
     price = models.IntegerField("цена товара", blank=True, null=True)
     rate = models.FloatField("рейтинг товара", blank=True, null=True)
 
