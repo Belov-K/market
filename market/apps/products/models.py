3075cf8f2ff8f96e4ebd909206cc3481dcc66ae4
kiril <kaz1nak3@mail.ru>
2023-06-19 09:39:17 +0300

    feat(products): создал продукт и отзыв

--- market/apps/products/models.py
+++ market/apps/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    product_title=models.CharField('название продукта', max_length=200)
+    product_text=models.TextField('описание продукта')
+    pub_date=models.DateTimeField('дата публикации продукта')
+
+class Review(models.Model):
+    product=models.ForeignKey(Product, on_delete=models.CASCADE)
+    author_name=models.CharField('автор отзыва', max_length=50)
+    review_text=models.TextField('текст отзыва')
