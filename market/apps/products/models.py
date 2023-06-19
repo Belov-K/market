5cc7fffe039ea5575eb0bb21fc130a6795a013ef
kiril <kaz1nak3@mail.ru>
2023-06-19 10:11:18 +0300

    fix(products): поменял названия

--- market/apps/products/models.py
+++ market/apps/products/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Product(models.Model):
-    product_title=models.CharField('название продукта', max_length=200)
-    product_text=models.TextField('описание продукта')
-    pub_date=models.DateTimeField('дата публикации продукта')
+    product_title=models.CharField('название товара', max_length=200)
+    product_text=models.TextField('описание товара')
+    pub_date=models.DateTimeField('дата публикации товара')
 
 class Review(models.Model):
     product=models.ForeignKey(Product, on_delete=models.CASCADE)
