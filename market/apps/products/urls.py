ed46e5928b02a56335dea5970cfb76c8d671adce
kiril <kaz1nak3@mail.ru>
2023-06-19 13:33:43 +0300

    fix(products): добавил вывод описания товара

--- market/apps/products/urls.py
+++ market/apps/products/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import  views
 
+app_name='products'
 urlpatterns=[
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:product_id>/', views.detail, name='detail'),
+    path('<int:product_id>/leave_review/', views.leave_review, name='leave_review'),
 ]
--- market/apps/products/views.py
+++ market/apps/products/views.py
@@ -1,4 +1,30 @@
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
 
+from django.urls import reverse
+
+from .models import Product
+
 def index(request):
-    return render(request, 'products/list.html')
+    latest_products_list=Product.objects.order_by('-pub_date')[:5]
+    return render(request, 'products/list.html', {'latest_products_list': latest_products_list})
+
+def detail(request, product_id):
+    try:
+        a=Product.objects.get(id=product_id)
+    except:
+        raise Http404("Товар не найден")
+
+    latest_review_list=a.review_set.order_by('-id')[:10]
+
+    return render(request, 'products/detail.html', {'product': a, 'latest_review_list': latest_review_list})
+
+def leave_review(request, product_id):
+    try:
+        a=Product.objects.get(id=product_id)
+    except:
+        raise Http404("Товар не найден")
+
+    a.review_set.create(author_name=request.POST['name'], review_text=request.POST['text'])
+
+    return HttpResponseRedirect(reverse('products:detail', args=(a.id,)))
