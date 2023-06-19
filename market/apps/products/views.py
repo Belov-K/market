79d07b1033fdb02a95b115de6c796798448e8c67
kiril <kaz1nak3@mail.ru>
2023-06-19 11:39:19 +0300

    feat(products): изменил вывод текста и название страницы

--- market/apps/products/views.py
+++ market/apps/products/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-def index(reques):
-    return HttpResponse("1")
+def index(request):
+    return render(request, 'products/list.html')
